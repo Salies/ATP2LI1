6e5df8c38d5026d846ee9f74374b95063e2ea1a4
Salies <serezane@protonmail.com>
2020-11-25 01:39:11 -0300

    Exercício 6

--- e6.c
+++ e6.c
@@ -13,12 +13,28 @@ int main(void){
     int PolTeste2[3] = {3, 2, 1};
     int PolTeste3[101], n3;
 
+    printf("Polinomio 1: ");
     ImprimePol(PolTeste, 4);
     printf("\n");
+    printf("Polinomio 2: ");
     ImprimePol(PolTeste2, 3);
-    printf("\n");
+    printf("\n\n");
+
+    printf("Soma de 1 com 2: ");
     SomarPol(PolTeste, PolTeste2, PolTeste3, 4, 3, &n3);
     ImprimePol(PolTeste3, n3);
+    printf("\n\n");
+
+    printf("Multiplicacao de 1 com 2: ");
+    MultiPol(PolTeste, PolTeste2, PolTeste3, 4, 3, &n3);
+    ImprimePol(PolTeste3, n3);
+    printf("\n\n");
+
+    printf("Se x = 3, Pol 1 = %d\n\n", ValorPolX(PolTeste, 4, 3));
+
+    printf("Pol 1 vezes a constante k = 2: ");
+    MultiCtePol(PolTeste, 4, 2);
+    ImprimePol(PolTeste, 4);
 
     return 0;
 }
@@ -48,6 +64,40 @@ void SomarPol (int *P1, int *P2, int *P3, int N1, int N2, int *N3){
     }
 
     *N3 = nMaior;
+
+    return;
+}
+
+void MultiPol (int *P1, int *P2, int *P3, int N1, int N2, int *N3){
+    *N3 = N1 + N2 - 1;
+
+    for(int i = 0; i < *N3; i++){ //prepara o arranjo final
+        *(P3 + i) = 0;
+    } 
+
+    for(int a = 0; a < N1; a++){
+        for(int b = 0; b < N2; b++){
+            *(P3 + a + b) += P1[a] * P2[b];
+        }
+    }
+
+    return;
+}
+
+int ValorPolX (int *P1, int N1, int X){
+    double v = 0;
+
+    for(int i = 0; i < N1; i++){
+        v += P1[i] * pow(X, i);
+    }
+
+    return (int) v;
+}
+
+void MultiCtePol (int *P1, int N1, int K){
+    for(int i = 0; i < N1; i++){
+        *(P1 + i) = K * P1[i];
+    }
 
     return;
 }
